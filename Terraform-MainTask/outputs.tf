c4d9bc415a860134311c5d957152e3c0e4318a83
S0ulTak3r <DanielBusinessBoguslavsky@gmail.com>
2023-10-31 13:26:52 +0200

    Added more Outputs

--- Terraform-MainTask/outputs.tf
+++ Terraform-MainTask/outputs.tf
@@ -1,9 +1,43 @@
 # Outputs the VPC IDs
-
 output "vpc_1_id" {
   value = aws_vpc.vpc_1.id
+  description = "The ID of the first VPC"
 }
 
 output "vpc_2_id" {
   value = aws_vpc.vpc_2.id
+  description = "The ID of the second VPC"
+}
+
+# Outputs the EC2 Instances IDs
+output "ec2_instance_1_id" {
+  value = aws_instance.instance_region1.id
+  description = "The ID of the first EC2 instance"
+}
+
+output "ec2_instance_2_id" {
+  value = aws_instance.instance_region2.id
+  description = "The ID of the second EC2 instance"
+}
+
+# Outputs the EC2 Instances Public IPs
+output "ec2_instance_1_public_ip" {
+  value = aws_instance.instance_region1.public_ip
+  description = "The public IP of the first EC2 instance"
+}
+
+output "ec2_instance_2_public_ip" {
+  value = aws_instance.instance_region2.public_ip
+  description = "The public IP of the second EC2 instance"
+}
+
+# Outputs the Security Groups IDs
+output "security_group_1_id" {
+  value = aws_security_group.security_group_region1.id
+  description = "The ID of the first security group"
+}
+
+output "security_group_2_id" {
+  value = aws_security_group.security_group_region2.id
+  description = "The ID of the second security group"
 }
