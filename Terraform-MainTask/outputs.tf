978c4fafaece459a8049dc44dc5833a1e17a1a2c
S0ulTak3r <DanielBusinessBoguslavsky@gmail.com>
2023-10-31 13:43:46 +0200

    edited output, to output the private IP's and not the public ones.

--- Terraform-MainTask/outputs.tf
+++ Terraform-MainTask/outputs.tf
@@ -20,16 +20,17 @@ output "ec2_instance_2_id" {
   description = "The ID of the second EC2 instance"
 }
 
-# Outputs the EC2 Instances Public IPs
-output "ec2_instance_1_public_ip" {
-  value = aws_instance.instance_region1.public_ip
-  description = "The public IP of the first EC2 instance"
+# Outputs the EC2 Instances Private IPs
+output "ec2_instance_1_private_ip" {
+  value = aws_instance.instance_region1.private_ip
+  description = "The private IP of the first EC2 instance"
 }
 
-output "ec2_instance_2_public_ip" {
-  value = aws_instance.instance_region2.public_ip
-  description = "The public IP of the second EC2 instance"
+output "ec2_instance_2_private_ip" {
+  value = aws_instance.instance_region2.private_ip
+  description = "The private IP of the second EC2 instance"
 }
+
 
 # Outputs the Security Groups IDs
 output "security_group_1_id" {
